624719a9d6fc1cf336edb211894cb1281eccdd25
amethyst-asuka <amethyst.asuka@gcmodeller.org>
2016-02-07 13:42:19 +0800

    all test ok!

--- Test.Project/Form1.vb
+++ Test.Project/Form1.vb
@@ -1,14 +1,14 @@
 ﻿Public Module Form1
 
 
-    Dim nnn As Global.NotifyOsd.ProcessBarBubble
+    Dim nnn As NotifyOsd.Framework.Balloon.ProgressBar
 
     Sub Main()
 
-        nnn = New NotifyOsd.ProcessBarBubble(New NotifyOsd.OsdNotifier.Message With {.Title = "测试消息头333333", .Message = "44444收到甲方监理考试的\n恢复健康绿色的很快就就考虑实际发生\n444444444\ngjgfdgdfjljkldssdsddd\nhkhhhhhhhhhhhhhhhhh\nhhhhhhhhhhhhhhhhhhhh"}, New Point)
+        nnn = New NotifyOsd.Framework.Balloon.ProgressBar(New NotifyOsd.Message With {.Title = "测试消息头333333", .Message = "44444收到甲方监理考试的\n恢复健康绿色的很快就就考虑实际发生\n444444444\ngjgfdgdfjljkldssdsddd\nhkhhhhhhhhhhhhhhhhh\nhhhhhhhhhhhhhhhhhhhh"}, New Point)
         Call nnn.Show()
 
-        nnn.ProcessingBar.ProcBarAnimationSpeed = 995
+        nnn.ProgressBar.AnimationSpeed = 995
 
         For i As Integer = 1 To 100
 
@@ -18,7 +18,7 @@
         Next
 
 
-        Dim dddd As New NotifyOsd.ValueAdjustments(New NotifyOsd.OsdNotifier.Message With {.Title = "sfsdfsdfsdf", .Message = "sadddddddddddddddddddddddddddddddd\ndddddddddddddddddddddddd"},
+        Dim dddd As New NotifyOsd.Framework.Balloon.ValueAdjuster(New NotifyOsd.Message With {.Title = "sfsdfsdfsdf", .Message = "sadddddddddddddddddddddddddddddddd\ndddddddddddddddddddddddd"},
                                                    Sub(value) Console.WriteLine($"{NameOf(value)} ===> {value }"))
 
         Call dddd.Show()
@@ -26,6 +26,6 @@
         Call Console.ReadLine()
 
 
-        Call New NotifyOsd.OsdNotifier().SendMessage("test ok!", "test ok!", "", Nothing)
+        Call New NotifyOsd.Framework.Balloon.OsdNotifier().SendMessage("test ok!", "test ok!", "", Nothing)
     End Sub
 End Module
