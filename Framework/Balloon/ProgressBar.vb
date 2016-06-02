ac9a719b88c4c1e35c87ee9532744264a250f1a2
xieguigang <xie.guigang@gmail.com>
2016-06-03 03:14:46 +0800

    not sure which thread is not exit

--- Framework/Balloon/ProgressBar.vb
+++ Framework/Balloon/ProgressBar.vb
@@ -3,7 +3,7 @@ Imports NotifyOsd.BubblesDisplay
 
 Namespace Framework.Balloon
 
-    Public Class ProgressBar
+    Public Class ProgressBar : Implements IDisposable
 
         Public Property PercentageValue As Integer
             Get
@@ -61,5 +61,39 @@ Namespace Framework.Balloon
             Me._procBubble.ScreenOffSet = screenOffset
             Me._procBubble.Message = msg
         End Sub
+
+#Region "IDisposable Support"
+        Private disposedValue As Boolean ' To detect redundant calls
+
+        ' IDisposable
+        Protected Overridable Sub Dispose(disposing As Boolean)
+            If Not disposedValue Then
+                If disposing Then
+                    ' TODO: dispose managed state (managed objects).
+                    Call Me._procBubble.Dispose()
+                    Call Me._invokeThread.Abort()
+                End If
+
+                ' TODO: free unmanaged resources (unmanaged objects) and override Finalize() below.
+                ' TODO: set large fields to null.
+            End If
+            disposedValue = True
+        End Sub
+
+        ' TODO: override Finalize() only if Dispose(disposing As Boolean) above has code to free unmanaged resources.
+        'Protected Overrides Sub Finalize()
+        '    ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+        '    Dispose(False)
+        '    MyBase.Finalize()
+        'End Sub
+
+        ' This code added by Visual Basic to correctly implement the disposable pattern.
+        Public Sub Dispose() Implements IDisposable.Dispose
+            ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+            Dispose(True)
+            ' TODO: uncomment the following line if Finalize() is overridden above.
+            ' GC.SuppressFinalize(Me)
+        End Sub
+#End Region
     End Class
 End Namespace
--- Test.Project/Program.vb
+++ Test.Project/Program.vb
@@ -3,12 +3,12 @@
 Public Module Program
 
     Sub Main()
-        Call Threading.Thread.Sleep(2 * 1000)
-        Call New NotifyOsd.Framework.Balloon.OsdNotifier().SendMessage("test ok!", "test ok!", "", Nothing)
+        'Call Threading.Thread.Sleep(2 * 1000)
+        'Call New NotifyOsd.Framework.Balloon.OsdNotifier().SendMessage("test ok!", "test ok!", "", Nothing)
 
-        Dim gg As Byte(,) = New Byte(500, 500) {}
-        '  Dim iii = TestPointer(100, 100, gg)
-        Pause()
+        'Dim gg As Byte(,) = New Byte(500, 500) {}
+        ''  Dim iii = TestPointer(100, 100, gg)
+        'Pause()
 
         Dim msg As New NotifyOsd.Message With {
             .Title = "进度条气泡测试(示例代码)",
@@ -23,32 +23,35 @@ For i As Integer = 1 To 100
     Call Threading.Thread.Sleep(300)
 Next"
         }
-        Dim proBar = New NotifyOsd.Framework.Balloon.ProgressBar(msg, New Point)
-        Call proBar.Show()
+        Using proBar = New NotifyOsd.Framework.Balloon.ProgressBar(msg, New Point)
+            Call proBar.Show()
 
-        proBar.ProgressBar.AnimationSpeed = 995
+            proBar.ProgressBar.AnimationSpeed = 995
 
-        For i As Integer = 1 To 100
-            proBar.PercentageValue = i
-            Call Threading.Thread.Sleep(300)
-        Next
+            For i As Integer = 1 To 100
+                proBar.PercentageValue = i
+                Call Threading.Thread.Sleep(30)
+            Next
+
+        End Using
+
+        Call App.Exit()
+
+        '        msg = New NotifyOsd.Message With {
+        '            .Title = "调节测试(在气泡上滚动鼠标滚轮)",
+        '            .Message = "这个消息气泡可以用作为音量调节或者数值调节的工具
+
+        'Dim valueAdjust As New ValueAdjuster(msg, AddressOf adjustInvoke)
+        'valueAdjust.Show()
+        'valueAdjust.PercentageValue = 50
+        '"
+        '        }
+        '        Dim valueAdjust As New NotifyOsd.Framework.Balloon.ValueAdjuster(msg, AddressOf adjustInvoke)
+        '        valueAdjust.Show()
+        '        valueAdjust.PercentageValue = 50
 
 
-        msg = New NotifyOsd.Message With {
-            .Title = "调节测试(在气泡上滚动鼠标滚轮)",
-            .Message = "这个消息气泡可以用作为音量调节或者数值调节的工具
-            
-Dim valueAdjust As New ValueAdjuster(msg, AddressOf adjustInvoke)
-valueAdjust.Show()
-valueAdjust.PercentageValue = 50
-"
-        }
-        Dim valueAdjust As New NotifyOsd.Framework.Balloon.ValueAdjuster(msg, AddressOf adjustInvoke)
-        valueAdjust.Show()
-        valueAdjust.PercentageValue = 50
-
-
-        Call Console.ReadLine()
+        '        Call Console.ReadLine()
 
     End Sub
 
