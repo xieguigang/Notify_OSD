a350778169b983f62f1f4707df2a4fbe60bf2e20
xieguigang <xie.guigang@gcmodeller.org>
2020-08-14 19:16:25 +0800

    add resource files

---
+++ NotifyOsd/Message.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Net.Http
+
+Public Class Message
+
+    Public Property title As String
+    Public Property content As String
+    Public Property icon As String
+    Public Property sound As String
+    Public Property behaviors As BubbleBehaviors
+
+    Public Function GetIconImage() As Image
+        Return DataURI.URIParser(icon).ToStream.DoCall(AddressOf Image.FromStream)
+    End Function
+End Class
+
