c779b93449541a970309e65a62d8f21c415798f8
xieguigang <xie.guigang@gcmodeller.org>
2020-08-15 13:13:22 +0800

    render message

--- NotifyOsd/MessageRender.vb
+++ NotifyOsd/MessageRender.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Drawing
+Imports System.Drawing.Drawing2D
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Serialization.JSON
 
@@ -7,29 +8,27 @@ Module MessageRender
     ''' <summary>
     ''' Drawing the 8 parts of the image frame into one piece
     ''' </summary>
-    ''' <param name="ImageData"></param>
+    ''' <param name="imageData"></param>
     ''' <returns></returns>
     ''' <remarks></remarks>
-    Public Function DrawFrame(ImageData As Image) As Image
-        Dim resIMG As Image = DirectCast(ImageData.Clone, Image)
+    Public Function DrawFrame(imageData As Image) As Image
+        Using g As Graphics2D = DirectCast(imageData.Clone, Image)
+            Dim bottomHeight As Integer = My.Resources.NotificationMainPanel_bottom.Height,
+                   topHeight As Integer = My.Resources.NotificationMainPanel_top.Height
 
-        Using GraphicDevice As Graphics = Graphics.FromImage(resIMG)
-            Dim BottomHeight As Integer = My.Resources.NotificationMainPanel_bottom.Height,
-                   TopHeight As Integer = My.Resources.NotificationMainPanel_top.Height
+            g.CompositingQuality = CompositingQuality.HighQuality
 
-            GraphicDevice.CompositingQuality = Drawing2D.CompositingQuality.HighQuality
+            Call g.DrawImage(My.Resources.NotificationMainPanel_top, 7, 0, imageData.Width - 14, topHeight)
+            Call g.DrawImage(My.Resources.NotificationMainPanel_top_LEFT, 0, 0, 7, 7)
+            Call g.DrawImage(My.Resources.NotificationMainPanel_top_RIGHT, imageData.Width - 7, 0, 7, 7)
+            Call g.DrawImage(My.Resources.NotificationMainPanel_bottom, 7, imageData.Height - bottomHeight, imageData.Width - 14, bottomHeight)
+            Call g.DrawImage(My.Resources.NotificationMainPanel_bottom_LEFT, 0, imageData.Height - 7, 7, 7)
+            Call g.DrawImage(My.Resources.NotificationMainPanel_bottom_Right, imageData.Width - 7, imageData.Height - 7, 7, 7)
 
-            Call GraphicDevice.DrawImage(My.Resources.NotificationMainPanel_top, 7, 0, ImageData.Width - 14, TopHeight)
-            Call GraphicDevice.DrawImage(My.Resources.NotificationMainPanel_top_LEFT, 0, 0, 7, 7)
-            Call GraphicDevice.DrawImage(My.Resources.NotificationMainPanel_top_RIGHT, ImageData.Width - 7, 0, 7, 7)
-            Call GraphicDevice.DrawImage(My.Resources.NotificationMainPanel_bottom, 7, ImageData.Height - BottomHeight, ImageData.Width - 14, BottomHeight)
-            Call GraphicDevice.DrawImage(My.Resources.NotificationMainPanel_bottom_LEFT, 0, ImageData.Height - 7, 7, 7)
-            Call GraphicDevice.DrawImage(My.Resources.NotificationMainPanel_bottom_Right, ImageData.Width - 7, ImageData.Height - 7, 7, 7)
+            Call g.DrawImage(My.Resources.PanelFrame, 0, topHeight, My.Resources.PanelFrame.Width, imageData.Height - topHeight - bottomHeight)
+            Call g.DrawImage(My.Resources.PanelFrame, imageData.Width - My.Resources.PanelFrame.Width, topHeight, My.Resources.PanelFrame.Width, imageData.Height - topHeight - bottomHeight)
 
-            Call GraphicDevice.DrawImage(My.Resources.PanelFrame, 0, TopHeight, My.Resources.PanelFrame.Width, ImageData.Height - TopHeight - BottomHeight)
-            Call GraphicDevice.DrawImage(My.Resources.PanelFrame, ImageData.Width - My.Resources.PanelFrame.Width, TopHeight, My.Resources.PanelFrame.Width, ImageData.Height - TopHeight - BottomHeight)
-
-            Return resIMG
+            Return g.ImageResource
         End Using
     End Function
 
@@ -41,12 +40,12 @@ Module MessageRender
     ''' <remarks>函数依靠字符串中含有多少个回车符来判断绘制的位置</remarks>
     Public Function DrawMessage(MSG As Message, Renderer As RenderParameters) As Image
         Try
-            Return __drawInner(MSG, Renderer)
+            Return drawMessageInternal(MSG, Renderer)
         Catch ex As Exception
             ex = New Exception("[Message] " & MSG.GetJson, ex)
             ex = New Exception("[Parameters] " & Renderer.GetJson, ex)
 
-            Dim exMsg As String = App.BugsFormatter(ex)
+            Dim exMsg As String = VBDebugger.BugsFormatter(ex)
             Call FileIO.FileSystem.WriteAllText(App.HOME & "/notify-osd.log", exMsg & vbCrLf, append:=True)
 
             Return Nothing
@@ -56,9 +55,9 @@ Module MessageRender
     ReadOnly _textMeasures As Graphics = Graphics.FromImage(My.Resources.UBUNTU)
     ReadOnly _minSize As New Size(352, 73)
 
-    Private Function __drawInner(msg As Message, params As RenderParameters) As Image
-        Dim title As String = If(String.IsNullOrEmpty(msg.title), Now.ToString, msg.title.Replace("\n", vbCrLf)),
-            message As String = If(String.IsNullOrEmpty(msg.Message), "", msg.Message.Replace("\n", vbCrLf))
+    Private Function drawMessageInternal(msg As Message, params As RenderParameters) As Image
+        Dim title As String = If(String.IsNullOrEmpty(msg.title), Now.ToString, msg.title.Replace("\n", vbCrLf))
+        Dim message As String = If(String.IsNullOrEmpty(msg.content), "", msg.content.Replace("\n", vbCrLf))
         Dim titleSize As SizeF = _textMeasures.MeasureString(title, params.TitleFont)
         Dim msgSize As SizeF = _textMeasures.MeasureString(message, params.MessageFont)
         Dim margins As New Size(15, 5)
@@ -74,18 +73,17 @@ Module MessageRender
             grSize = New Point(352, grSize.Y)
         End If
 
-        Using grDraw As Graphics2D = New Size(grSize.X, grSize.Y + params.YDelta) _
-            .CreateGDIDevice(Color.Transparent)
+        Using g As Graphics2D = New Size(grSize.X, grSize.Y + params.YDelta).CreateGDIDevice(Color.Transparent)
+            g.Graphics.CompositingQuality = CompositingQuality.HighQuality
 
-            grDraw.Graphics.CompositingQuality = Drawing2D.CompositingQuality.HighQuality
             '绘制表面纹理
-            Call grDraw.Graphics.FillRegion(Brushes.Black, New Region(New Rectangle(New Point, grSize)))
+            Call g.FillRegion(Brushes.Black, New Region(New Rectangle(New Point, grSize)))
             '绘制消息内容
-            grDraw.Graphics.DrawImage(msg.icon, 15, 15, params.IconSize, params.IconSize)
-            grDraw.Graphics.DrawString(title, params.TitleFont, Brushes.WhiteSmoke, New Point(78, 15))
-            grDraw.Graphics.DrawString(message, params.MessageFont, Brushes.White, New Point(78, 5 + 15 + titleSize.Height))
+            g.DrawImage(msg.GetIconImage, 15, 15, params.IconSize, params.IconSize)
+            g.DrawString(title, params.TitleFont, Brushes.WhiteSmoke, New Point(78, 15))
+            g.DrawString(message, params.MessageFont, Brushes.White, New Point(78, 5 + 15 + titleSize.Height))
 
-            Return grDraw.ImageResource
+            Return g.ImageResource
         End Using
     End Function
 End Module
